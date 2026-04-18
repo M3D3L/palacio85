95ec0399fae9c4cedfb2fcc0b040d69e503675e2
gmedel <gmedel@alm.com>
2026-04-18 10:10:36 -0700

    fix: guard against undefined env in hydrogen.config.ts

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,14 +1,12 @@
 import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
-  shopify: ({env}) => ({
+  shopify: (req) => ({
     defaultCountryCode: 'MX',
     defaultLanguageCode: 'ES',
-    // These 'env' values pull directly from the list you see in Shopify Admin
-    storeDomain: env.PUBLIC_STORE_DOMAIN || 'palacio85.myshopify.com',
-    storefrontToken:
-      env.PUBLIC_STOREFRONT_API_TOKEN || '5353a2766a3513847f02be6445e5301d',
-    storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2022-07',
+    storeDomain: req?.env?.PUBLIC_STORE_DOMAIN || 'palacio85.myshopify.com',
+    storefrontToken: req?.env?.PUBLIC_STOREFRONT_API_TOKEN || '5353a2766a3513847f02be6445e5301d',
+    storefrontApiVersion: req?.env?.PUBLIC_STOREFRONT_API_VERSION || '2022-07',
   }),
   session: CookieSessionStorage('__session', {
     path: '/',
