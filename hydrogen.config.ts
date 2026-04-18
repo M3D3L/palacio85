352a34d98ca10b8248cb9fe9e09b6b68be1910d0
gmedel <gmedel@alm.com>
2026-04-17 21:12:34 -0700

    updated the build script

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,13 +1,15 @@
 import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
-  shopify: {
+  shopify: ({env}) => ({
     defaultCountryCode: 'MX',
     defaultLanguageCode: 'ES',
-    storeDomain: 'palacio85.myshopify.com',
-    storefrontToken: '5353a2766a3513847f02be6445e5301d',
-    storefrontApiVersion: '2022-07',
-  },
+    // These 'env' values pull directly from the list you see in Shopify Admin
+    storeDomain: env.PUBLIC_STORE_DOMAIN || 'palacio85.myshopify.com',
+    storefrontToken:
+      env.PUBLIC_STOREFRONT_API_TOKEN || '5353a2766a3513847f02be6445e5301d',
+    storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2022-07',
+  }),
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
